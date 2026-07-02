61562511fe2c81855bc8d30a70f3ae1720ae9154
Pham Xuan Tho <thopx@viva.com>
2026-07-02 15:13:51 +0700

    add migration to delete recently imported machines on VPS

---
+++ database/migrations/2026_07_02_151258_delete_recently_imported_machines_on_vps.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $timestamps = ['2026-07-02 14:53:20', '2026-07-02 14:53:21'];
+        \Illuminate\Support\Facades\DB::table('machines')
+            ->whereIn('created_at', $timestamps)
+            ->delete();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
